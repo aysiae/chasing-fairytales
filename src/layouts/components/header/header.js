ecf119d3e05c7d78386ee2ae614516cb3e0ec6ae
Aysia Brown <aysiaebrown@gmail.com>
2021-02-24 16:33:07 -0800

    made header

---
+++ src/layouts/components/header/header.js
@@ -0,0 +1,15 @@
+import './header.scss'
+
+function Header () {
+
+    return(
+        <div className='header'>
+            <img src="https://img.icons8.com/color/96/000000/fenix.png"/>
+            <h1>Underworld Corp.</h1>
+        </div>
+    )
+}
+
+// reference for phoenix img 
+{/* <a href="https://icons8.com/icon/57197/phoenix">phoenix icon by Icons8</a> */}
+export default Header;
--- src/layouts/home/home.js
+++ src/layouts/home/home.js
@@ -1,7 +1,14 @@
+import Header from '../components/header/header'
+import Footer from '../components/footer/footer'
+import Nav from '../components/navigation/navigation'
+
 function Home () {
     return (
         <div>
+            <Header />
+            <Nav/>
             <p>HOME!!!</p>
+            <Footer />
         </div>
     )
 }
