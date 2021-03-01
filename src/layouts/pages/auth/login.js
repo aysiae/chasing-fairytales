48e08f87f2fd78fdf86db0319bf0e3554e6a2fde
Aysia Brown <aysiaebrown@gmail.com>
2021-03-01 12:42:02 -0800

    started login page

--- src/layouts/pages/auth/login.js
+++ src/layouts/pages/auth/login.js
@@ -1,6 +1,21 @@
+import './login.scss'
+
 function Login () {
 
-    
+    return(
+        <>
+        <div className='login'>
+            <h2>Login:</h2>
+            <form style={{marginTop: '50px'}}>
+                <label className='username'>Username:</label>
+                <input className='username' type='text'></input> {' '}
+                <label className='password'>Password:</label>
+                <input className='password' type='password'></input>
+                <button>Go</button>
+            </form>
+        </div>
+        </>
+    )
     
 }
 
