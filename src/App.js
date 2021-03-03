9edbf2bdc9b0b21392576a32e8e19a4f68c46ff9
Aysia Brown <aysiaebrown@gmail.com>
2021-03-03 11:56:56 -0800

    started character sheet form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from './layouts/pages/home/home'
 import HeroHeader from './layouts/components/header/hero-header.js'
 import Login from './layouts/pages/auth/login'
+import CharacterForm from './layouts/pages/characters/character-form'
 import PrivateRoutes from './routes/private'
 import {
   BrowserRouter as Router,
@@ -16,9 +17,12 @@ function App() {
       <Route exaxt path='/test'>
           <HeroHeader/>
       </Route>
-      <PrivateRoutes path='/' component={Home}/>
+      <PrivateRoutes exact path='/' component={Home}/>
       <Route exact path='/login'>
         <Login />
+      </Route>
+      <Route exact path ='/character/add'>
+          <CharacterForm/>
       </Route>
     </Router>
     </>
