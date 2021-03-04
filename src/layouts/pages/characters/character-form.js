44320cf21e4b29e5563bf7e016fbc93a878525c7
Aysia Brown <aysiaebrown@gmail.com>
2021-03-03 18:29:29 -0800

    finished creating char sheet form

--- src/layouts/pages/characters/character-form.js
+++ src/layouts/pages/characters/character-form.js
@@ -1,13 +1,38 @@
+import { useState } from 'react';
 import Header from '../../components/header/header'
 import './character-form.scss'
 
 function CharacterForm() {
+    // immutable data
     const starSigns = ['None','Aries','Taurus','Gemini','Cancer','Leo','Virgo','Libra','Scorpio','Sagitarius','Capricorn','Aquarius','Pisces'];
     const mbti = ['None', 'INTJ - The Architect', 'INTP - The Logician', 'ENTJ - The Commander', 'ENTP - The Debater',
     'INFJ - The Advocate', 'INFP - The Mediator', 'ENFJ - The Protagonist', 'ENFP - The Campaigner',
     'ISTJ - The Logistician', 'ISFJ - The Defender', 'ESTJ - The Executive', 'ESFJ - The Consul',
     'ISTP - The Virtuoso', 'ISFP - The Adventurer', 'ESTP - The Entrepreneur', 'ESFP - The Entertainer']
     const morals = ['None', 'Lawful Neutral', 'True Neutral', 'Chaotic Neutral', 'Lawful Good', 'True Good', 'Chaotic Good','Lawful Evil', 'True Evil','Chaotic Evil'];
+    const loveType = ['None', 'Philia','Pragma','Storge','Ludus','Eros','Mania','Philautia','Agape'];
+    const loveLang = ['None','Words of Affirmation', 'Physical Touch','Quality Time','Recieving Gifts','Acts of Service'];
+    const relationship = ['Single','In Relationship','Married','It\'s complicated'];
+    const gender = ['male', 'female','non-binary']
+    // state 
+    const [inRelationship, setInRelationship] = useState(false)
+    const [isSuper, setIsSuper] = useState(false);
+
+    const handleInRelationship = (value) => {
+        if(value !== 'Single') {
+            setInRelationship(true);
+        } else {
+            setInRelationship(false);
+        }
+    }
+
+    const handleIsSuper = (value) => {
+        if(value === 'Yes') {
+            setIsSuper(true);
+        } else {
+            setIsSuper(false);
+        }
+    }
 
 
     return(
@@ -33,6 +58,12 @@ function CharacterForm() {
                     <input type='text'></input>
                     <label>Age</label>
                     <input type='number' min='1'></input>
+                    <label>Gender:</label>
+                        <select>
+                            {gender.map(sex=>(
+                            <option value={sex}>{sex}</option>
+                            ))}
+                        </select>
                     <label>Occupation</label>
                     <input type='text'></input>
                     
@@ -96,8 +127,55 @@ function CharacterForm() {
 
                     <h3>Love & Romance</h3>
                     <label>Love Type:</label>
+                    <select>
+                        {loveType.map(type => (
+                            <option value={type}>{type}</option>
+                        ))}
+                    </select>
+                    <label>Love Languages (limit 2):</label>
+                    {loveLang.map(type=> (
+                        <span>
+                        <input type='checkbox' value={type}/>
+                        <label>{type}</label>
+                        </span>
+                    ))
+                    }
+                    <label>Relationship Status:</label>
+                    <select onChange={(e) => handleInRelationship(e.target.value)}>
+                        {relationship.map(status => (
+                            <option value={status}>{status}</option>
+                        ))}
+                    </select>
+                    {inRelationship ? 
+                    <>
+                    <label>With:</label>
                     <input type='text'></input>
+                    </>
+                : null}
 
+                <h3>Supernatural</h3>
+                <label>Is supernatural?</label>
+                <select onChange={(e) => handleIsSuper(e.target.value)}>
+                    <option value='No'>No</option>
+                    <option value='Yes'>Yes</option>
+                </select>
+                { isSuper ?
+                <>
+                <label>Supernatural Type:</label>
+                <input type='text'></input>
+                <label>Supernatural Species/Being Description:</label>
+                <input type='textarea'></input>
+                <label>Supernatural Powers:</label>
+                <input type='textarea'></input>
+                </>
+                : null
+                }
+
+                <h3>Biography:</h3>
+                <input type='textarea'></input>
+                
+                <h3>Additional Notes:</h3>
+                <input type='textarea'></input>
                 </fieldset>
             </form>
             
