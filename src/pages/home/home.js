12f9dec33b001e14c4996e4408c7257a4580113d
Aysia Brown <aysiaebrown@gmail.com>
2021-04-27 15:00:37 -0700

    static load of characters on homepage

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -13,16 +13,34 @@ import './home.scss'
 
 // redux states 
 import {get} from '../../redux/reducers/oneliners';
-
+import {getChars} from '../../redux/reducers/characters';
+import {getAll} from '../../firebase/database/chars'
 
 const mapStateToProps = state => ({
-    oneliners: state.oneliners.oneliners
+    oneliners: state.oneliners.oneliners,
+    chars: state.characters.characters
 })
 
-const mapDispatchToProps = {get}
+const mapDispatchToProps = {get, getChars}
 
 // functional component starts here 
 function Home (props) {
+    const placeholderImg = 'https://images.unsplash.com/photo-1544502062-f82887f03d1c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2027&q=80';
+    const [uid, setUID] = useState(document.cookie.split(';')[0].split('=')[1]);
+
+    const toggle = () => {
+        console.log(props.chars)
+    }
+
+    const loadCharacters = async () => {
+        let chars = await getAll(uid);
+        props.getChars(chars); 
+        console.log('chars', props.chars);
+
+    }
+
+    useEffect(loadCharacters,[])
+    
 
 
     return (
@@ -31,7 +49,20 @@ function Home (props) {
             <main className='home-dash'>
                 <OnelinerPrompts/>
                 <div id='home-chars'>
-                    
+                    <h3>Quick Characters</h3>
+                    <div>
+                        {props.chars.map((char, idx) => {
+                            
+                            if(idx <= 8) {
+                                return (
+                                    <span className='char-preview'>
+                                        <img src={char.charSheet.img ? char.charSheet.img : placeholderImg}/>
+                                        <h4>{char.charSheet.firstName}</h4>
+                                    </span>
+                                )
+                            }
+                            })}
+                    </div>
                 </div>
                 <div id='home-universe'>
 
